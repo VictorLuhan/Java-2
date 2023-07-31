33c46dfba8d27a61f8cbb1620213ddb936a40bf5
Victor Luhan <92903765+VictorLuhan@users.noreply.github.com>
2023-07-31 17:22:00 -0300

    Add files via upload

---
+++ Exercicio1/Main.java
@@ -0,0 +1,23 @@
+package Main;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a;
+		int b;
+		int soma;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		soma = a + b;
+		
+		System.out.printf("SOMA = "+ soma);
+		
+
+	}
+
+}
