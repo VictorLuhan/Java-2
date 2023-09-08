9f3db6097e299a8cb0d80847f3a940c12365348f
Victor Luhan <92903765+VictorLuhan@users.noreply.github.com>
2023-09-08 15:59:30 -0300

    Add files via upload

---
+++ ExMatrizes/src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		 System.out.print("Digite o número de linhas da matriz (M): ");
+	        int M = scanner.nextInt();
+	        System.out.print("Digite o número de colunas da matriz (N): ");
+	        int N = scanner.nextInt();
+
+	}
+
+}
